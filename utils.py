a1eb7f97904c283db28079e4baa9df74ace58793
sg670w <sg670w@att.com>
2025-01-17 15:57:58 -0600

    autodetect gpu

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 
 import time
+import os
+import sys
 
 def timing_decorator(func):
     def wrapper(*args, **kwargs):
@@ -7,7 +9,7 @@ def timing_decorator(func):
         result = func(*args, **kwargs)
         end_time = time.time()
         execution_time = end_time - start_time
-        print(f"Execution time for {func.__name__} is {execution_time} seconds")
+        print(f"Execution time for {func.__name__} is {execution_time:.4f} seconds")
         return result
     return wrapper
 
@@ -22,6 +24,34 @@ def avg_timing_decorator(func):
             execution_time = end_time - start_time
             total_time += execution_time
         avg_time = total_time / N
-        print(f"Average execution time for {func.__name__} is {avg_time} seconds")
+        print(f"Average execution time for {func.__name__} is {avg_time:.4f} seconds")
         return result
     return wrapper
+
+def suppress_output():
+    sys.stdout = open(os.devnull, 'w')
+
+def enable_output():
+    sys.stdout = sys.__stdout__
+    
+def check_numba_cuda():
+    try:
+        import numba
+        from numba import cuda
+        cuda.detect()
+        return True
+    except:
+        return False
+    
+def check_openCl():
+    try:
+        import pyopencl as cl
+        suppress_output()
+        os.environ['PYOPENCL_CTX'] = '0'
+        cl.create_some_context()
+        enable_output()
+        return True
+    except:
+        enable_output()
+        return False
+    
