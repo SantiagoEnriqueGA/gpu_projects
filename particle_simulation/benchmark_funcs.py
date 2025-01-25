d534c6129035fa3df9fe2b2cc6aac1ada399a2f3
SantiagoEnriqueGA <sega97@gmail.com>
2025-01-24 18:52:55 -0600

    benchmarks

--- particle_simulation/benchmark_funcs.py
+++ particle_simulation/benchmark_funcs.py
@@ -120,13 +120,13 @@ print(benchmark(handle_boundary_collisions_cudaKernel, (positions_cp, velocities
 
 
 # OUTPUT:
-# Benchmarking 5,000 particles in a 200.00 x 200.00 space with 10 runs each.                                                             ents/Projects/cud
+# Benchmarking 5,000 particles in a 200.00 x 200.00 space with 10 runs each.
 # ----------------------------------------------------------------------------------------------------
 
 # Benchmarking gravitational force computations...
 # compute_forces_np average time:                         5.86001 seconds (std_dev: 0.02034 seconds)
 # compute_forces_cp average time:                         5.85390 seconds (std_dev: 0.00070 seconds)
-# compute_forces_cudaKernel average time:                 0.19990 seconds (std_dev: 0.00053 seconds)                                      (min: 47849.472 
+# compute_forces_cudaKernel average time:                 0.19990 seconds (std_dev: 0.00053 seconds)
 # ----------------------------------------------------------------------------------------------------
 # Speedup:                                                29.31x
 
@@ -134,13 +134,13 @@ print(benchmark(handle_boundary_collisions_cudaKernel, (positions_cp, velocities
 # handle_particle_collisions_np average time:             2.83940 seconds (std_dev: 0.46629 seconds)
 # handle_particle_collisions_cp average time:             2.73070 seconds (std_dev: 0.28245 seconds)
 # handle_particle_collisions_cKDTree average time:        0.32470 seconds (std_dev: 0.06981 seconds)
-# handle_particle_collisions_cudaKernel average time:     0.02270 seconds (std_dev: 0.00415 seconds)                                     ents/Projects/cud
+# handle_particle_collisions_cudaKernel average time:     0.02270 seconds (std_dev: 0.00415 seconds)
 # ----------------------------------------------------------------------------------------------------
 # Speedup:                                                125.09x
 
 # handle_boundary_collisions average time:                0.16140 seconds (std_dev: 0.48020 seconds)
 # handle_boundary_collisions_cudaKernel average time:     0.00010 seconds (std_dev: 0.00030 seconds)
-#                                                                                                                                         (min: 63604.767 
+
 # Benchmarking CuPy specific functions...
 # compute_forces_cudaKernel:              CPU:   221.151 us   +/- 52.04`5 (min:   132.900 / max:   358.000) us     GPU-0: 76154.183 us   +/- 3094.332 (min: 69212.318 / max: 83601.852) us
 # handle_particle_collisions_cudaKernel:  CPU:   157.586 us   +/- 41.083 (min:   112.800 / max:   307.400) us     GPU-0: 24926.697 us   +/- 1309.562 (min: 22241.920 / max: 26700.640) us
